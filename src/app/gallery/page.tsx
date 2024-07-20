c5154c3bf53a78c8d73beb36064d0aa76ed9e7fb
Muhammad Ibad <ibad3572@gmail.com>
2024-07-20 23:07:05 +0500

    feat: Update Gallery page to display images from Cloudinary search results

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,33 +1,37 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Upload } from "lucide-react";
-import { CldUploadButton } from "next-cloudinary";
-import React, { useState } from "react";
+import UploadImg from "@/components/UploadImg";
+import React from "react";
+import cloudinary from "cloudinary";
+import Images from "@/components/Images";
 
-const Gallery = () => {
-    const [details, setDetails] = useState<string | undefined>("");
-
+const Gallery = async () => {
+    const results = await cloudinary.v2.search
+        .expression("resource_type:image")
+        .sort_by("created_at", "desc")
+        .with_field('tags')
+        .max_results(12)
+        .execute();
+    console.log(results);
     return (
         <div>
             <div className="flex justify-between pt-6 px-6 items-center">
                 <h1 className="text-4xl font-semibold pb-2">Gallery</h1>
                 <div>
-                    <Button asChild >
-                        <div className="flex gap-2 ">
-                            <Upload  size={18}/>
-                            <CldUploadButton
-                            
-                                uploadPreset="ml_default"
-                                onUpload={(result: any) => {
-                                    console.log(result);
-                                    setDetails(result?.info?.public_id);
-                                    console.log(details);
-                                    // { public_id, secure_url, etc }
-                                }}
-                            />
-                        </div>
-                    </Button>
+                    <UploadImg />
                 </div>
+            </div>
+            <div className="grid grid-cols-4 gap-4 mt-4 mx-4">
+                {results.resources.map((each: any) => (
+                    <Images
+                        key={each.public_id}
+                        src={each.public_id}
+                        width="400"
+                        height="300"
+                        sizes="100vw"
+                        alt="Description of my image"
+                        data={each}
+                        path="/gallery" 
+                    />
+                ))}
             </div>
         </div>
     );
