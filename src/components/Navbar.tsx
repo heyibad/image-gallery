c30a65afcf104bf0764f2015e9355ecadc525a90
Muhammad Ibad <ibad3572@gmail.com>
2024-07-20 23:13:18 +0500

    feat: Add Navbar component for improved navigation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+
+const Navbar = () => {
+  return (
+    <nav className="border-b ">
+    <div className="flex h-16 text-center text-2xl font-bold px-4 container mx-auto mt-6">
+        ImageGallery
+        <div className="ml-auto flex text-center space-x-4">
+            <Avatar>
+                <AvatarImage src="https://github.com/heyibad.png" />
+                <AvatarFallback>I</AvatarFallback>
+            </Avatar>
+        </div>
+    </div>
+</nav>
+  )
+}
+
+export default Navbar
