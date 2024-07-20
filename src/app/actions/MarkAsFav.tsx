80dce81821f7333bb03c639b76aaff0f99b2ae9d
Muhammad Ibad <ibad3572@gmail.com>
2024-07-20 23:06:45 +0500

    refactor: Update MarkAsFav action to handle toggling favorite status

--- src/app/actions/MarkAsFav.tsx
+++ src/app/actions/MarkAsFav.tsx
@@ -2,7 +2,20 @@
 import cloudinary from "cloudinary";
 import { revalidatePath } from "next/cache";
 
-export async function MarkAsFav({ publicId }: { publicId: string }) {
-    await cloudinary.v2.uploader.add_tag("favorite", [publicId]);
-    revalidatePath("/gallery")
+export async function MarkAsFav({
+    public_id,
+    isFavorite,
+    path
+}: {
+    public_id: string;
+    isFavorite: boolean;
+    path:string
+}) {
+    if (isFavorite) {
+        await cloudinary.v2.uploader.remove_tag("favorite", [public_id]);
+    } else {
+        await cloudinary.v2.uploader.add_tag("favorite", [public_id]);
+    }
+    await new Promise(resolve=> setTimeout(resolve,1000))
+    revalidatePath(path);
 }
