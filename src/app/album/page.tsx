2d196674ebfd1035d08a207688fd4b95eae7feb9
Muhammad Ibad <ibad3572@gmail.com>
2024-08-02 22:18:42 +0500

    Added an album page

---
+++ src/app/album/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import cloudinary from "cloudinary";
+import { AlbumCard, recivingProp } from "@/components/AlbumCard";
+const Album = async () => {
+    const { folders } = await cloudinary.v2.api.root_folders();
+    console.log(folders);
+    return (
+        <div>
+            <div className="flex justify-between pt-6 px-6 items-center">
+                <h1 className="text-4xl font-semibold pb-2">Albums</h1>
+            </div>
+            <div className="flex flex-wrap  items-center gap-4 mt-4 mx-4">
+                {folders.map((folder: recivingProp) => {
+                    return <AlbumCard key={folder.external_id} data={folder} />;
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Album;
---
+++ src/components/AlbumCard.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+export type recivingProp = {
+    name: string;
+    path: string;
+    external_id: string;
+};
+export function AlbumCard({
+    data: { name, path, external_id },
+}: {
+    data: recivingProp;
+}) {
+    return (
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>{name}</CardTitle>
+                <CardDescription>
+                    Avaliable Photos in {name} Album
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+               
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button asChild>
+                    <Link href={`/album/${name}`}>View Album</Link>
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+}
