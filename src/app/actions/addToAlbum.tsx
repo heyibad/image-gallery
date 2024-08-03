776e32ef48a130ecf29f14d89fed36dfdfb2f692
Muhammad Ibad <ibad3572@gmail.com>
2024-08-03 13:01:48 +0500

    Fixed an Action issue of public id prefix

--- src/app/actions/addToAlbum.tsx
+++ src/app/actions/addToAlbum.tsx
@@ -10,10 +10,17 @@ export async function addToAlbum({
         public_id: string;
     };
 }) {
-    const existingFolder = await cloudinary.v2.api.create_folder(folderName);
-
+    await cloudinary.v2.api.create_folder(folderName);
+    let parts = image.public_id.split("/");
+    console.log(parts);
+    if (parts.length > 1) {
+     parts=parts.slice(1);
+     console.log(parts);
+    }
+    const publidId = parts.join("/");
+    console.log(publidId);
     await cloudinary.v2.uploader.rename(
         image.public_id,
-        `${folderName}/${image.public_id}`
+        `${folderName}/${publidId}`
     );
 }
