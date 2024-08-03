f9c679c0b4be490415d2c2f5a125d34ce25c4375
Muhammad Ibad <ibad3572@gmail.com>
2024-08-03 13:05:52 +0500

    Added a nexted album page

---
+++ src/app/album/[id]/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import cloudinary from "cloudinary";
+import Main from "@/components/Main";
+
+type CustomPropType = {
+    params:{
+        id:string
+    }
+}
+const CustomAlbum = async ({params:{id}}:CustomPropType) => {
+    const results = await cloudinary.v2.search
+        .expression(`resource_type:image AND folder=${id}`)
+        .sort_by("created_at", "desc")
+        .with_field("tags")
+        .max_results(12)
+        .execute();
+
+    return (
+        <div>
+            <div className="flex justify-between pt-6 px-6 items-center">
+                <h1 className="text-4xl font-semibold pb-2">Album, {id}</h1>
+            
+            </div>
+            <Main data={results} />
+        </div>
+    );
+};
+
+export default CustomAlbum;
