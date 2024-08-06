c2df887a769a6dd5b1c38c6b7de7fbe522bc8fca
Muhammad Ibad <ibad3572@gmail.com>
2024-08-06 16:37:40 +0500

    fix: Update Dropdown component to fix rendering issue

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -27,7 +27,7 @@ export function DropDown(props:any) {
     const [open, setOpen] = useState(false);
     return (
         <div className="absolute top-2 left-2 ">
-            <DropdownMenu open={open} onOpenChange={setOpen}>
+            <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button variant="outline" className="h-8 w-12">
                         <Menu size={16} />
