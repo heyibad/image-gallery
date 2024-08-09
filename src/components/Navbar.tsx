9a7fb9af25002b8a5344ddcd1063ec656269d25d
Muhammad Ibad <ibad3572@gmail.com>
2024-08-09 23:04:17 +0500

    Fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ScanSearch } from "lucide-react";
 
 
 const Navbar = () => {
   return (
     <nav className="border-b ">
-    <div className="flex h-16 text-center text-2xl font-bold px-4 container mx-auto mt-6">
-        ImageGallery
+    <div className="flex h-16 text-center text-xl font-bold px-4 container mx-auto mt-6">
+       <ScanSearch className="mx-2  " size={28}/> 
+       <span>
+       ImageGallery
+       </span>
         <div className="ml-auto flex text-center space-x-4">
             <Avatar>
                 <AvatarImage src="https://github.com/heyibad.png" />
