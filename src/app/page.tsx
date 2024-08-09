090f495beed8aa5959b2983ec95131f37264f518
Muhammad Ibad <ibad3572@gmail.com>
2024-08-09 20:15:03 +0500

    Added a a new feature with seach by tag func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,15 @@ import UploadImg from "@/components/UploadImg";
 import React from "react";
 import cloudinary from "cloudinary";
 import Main from "@/components/Main";
-const Page = async () => {
+import { SearchForm } from "@/components/SearchForm";
+
+type PageProps = {
+    params: {};
+    searchParams: { search: string };
+};
+const Page = async ({searchParams:{search}}:PageProps) => {
     const results = await cloudinary.v2.search
-        .expression("resource_type:image")
+        .expression(`resource_type:image ${search ? `AND tags=${search}`:""}`)
         .sort_by("created_at", "desc")
         .with_field("tags")
         .max_results(12)
@@ -16,10 +22,11 @@ const Page = async () => {
         <div>
             <div className="flex justify-between pt-6 px-6 items-center">
                 <h1 className="pt-2 pl-8 text-3xl md:text-4xl font-semibold pb-3">Gallery</h1>
-                <div>
+                <div className="ml-12">
                     <UploadImg />
                 </div>
             </div>
+            <SearchForm searchVal={search} />
             <Main data={results} />
         </div>
     );
---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,39 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
+import { useRouter } from 'next/navigation'
+import { Search } from 'lucide-react'
+
+
+
+
+export const SearchForm = ({searchVal}:{searchVal:string}) => {
+    const router = useRouter();
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        router.replace(`/?search=${encodeURI(tag)}`)
+        router.refresh()
+    }
+    const [tag, setTag] = useState(searchVal ?? "");
+
+    useEffect(() => {
+        setTag(searchVal);
+    }, [searchVal]);
+  return (
+    <div className="my-5 ml-12 ">
+    <form
+        className=" flex gap-2 mr-6"
+        onSubmit={handleSubmit}
+    >
+      
+
+       <Input placeholder='Search By Tag' className="w-[60vw]" type="text" value={tag} onChange={(e)=> setTag(e.target.value)}/>
+        <Button  className=" w-10 px-2 h-10" type="submit">
+           <Search className='w-full'/>
+        </Button>
+    </form>
+</div>
+  )
+}
